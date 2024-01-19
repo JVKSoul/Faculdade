d0333d0b74b19e46a9b122a4476ec042edd50072
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-01-19 18:03:41 -0300

    fix: Outlet merge resolve

--- src/pages/outlet.tsx
+++ src/pages/outlet.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Outlet as OutletRouterDom } from "react-router-dom";
 
@@ -11,14 +10,3 @@ export default () => {
     </>
   );
 };
-=======
-import React from 'react'
-
-const Outlet = () => {
-  return (
-    <div>Oaaaaa</div>
-  )
-}
-
-export default Outlet
->>>>>>> 4f7925c9c8a0a5341bea26850c220cfb4b95ff09
