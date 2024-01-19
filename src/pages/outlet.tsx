3edda77aff7f30815e4ef6cc99369498f046afcc
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-01-19 17:55:46 -0300

    feat: outletl structure initial

---
+++ src/pages/outlet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet as OutletRouterDom } from "react-router-dom";
+
+export default () => {
+  return (
+    <>
+      <div className="w-full bg-cyan-400"> asdasd</div>
+      <OutletRouterDom />
+      <div className="w-full  bg-cyan-400"> asdasd</div>
+    </>
+  );
+};
