96e36dd1736fe3fb70215562c1546fe3936dfdb5
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-01-19 23:11:33 -0300

    feat(home): add image in component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Product from "../components/product";
 import FAKE_DATA from "../../mock/FAKE_DATA";
 import { IProduct } from "../../mock/interface";
+import promotionFirstPageImage from "../assets/promotionFirstPageImage.png"
 
 const Home = () => {
   const [productArray, setProductArray] = useState<IProduct[]>();
@@ -11,8 +12,14 @@ const Home = () => {
   });
   
   return (
-    <div className="bg-zinc-900  min-h-screen">
-      <div className="w-4/5 mx-auto flex flex-row flex-wrap gap-5">
+    <div className="bg-zinc-900  min-h-screen flex flex-col justify-center">
+      <div className="w-full h-[40rem] bg-cover bg-center flex items-start justify-center" style={{backgroundImage: `url(${promotionFirstPageImage})`}}>
+        <div className="bg-black/[.4] text-center px-10 py-5 mt-16 flex flex-col gap-5">
+          <p className="text-4xl">ESPECIAL</p>
+          <p className="text-4xl">PROMOÇÃO DE FÉRIAS</p>
+        </div>
+      </div>
+      <div className="w-4/5 mx-auto flex flex-row flex-wrap gap-5 mt-32">
         {productArray &&
           productArray.map((product, index) => {
             return (
