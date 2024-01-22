9c8194decb485c55b25ba54c4a9031dcdc5dec30
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-01-22 18:06:55 -0300

    feat(app): implement route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Outlet from './pages/outlet';
+import Product from './pages/product';
 // import Outlet from '@components/Outlet';
 // import Home from '@pages/Home';
 
@@ -12,6 +13,7 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<Outlet/>} >
                         <Route index element={<Home />} />
+                        <Route path="/product/:id" element={<Product/>} />
                     </Route>
                 </Routes>
         </>
