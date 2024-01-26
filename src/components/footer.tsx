d52e7b9681001dc872a695c6bb3ee69746d6ee72
JVKSoul <156980601+JVKSoul@users.noreply.github.com>
2024-01-25 21:42:06 -0300

    feat(Footer/Outlet): add component footer ? and implemented from Outlet

---
+++ src/components/footer.tsx
@@ -0,0 +1,112 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#222222] pt-10 sm:mt-10 pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium text-white">Coluna 1</h3>
+            <ul className="mt-4 space-y-4">
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium text-white">Coluna 2</h3>
+            <ul className="mt-4 space-y-4">
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium text-white">Coluna 3</h3>
+            <ul className="mt-4 space-y-4">
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-sm font-medium text-white">Coluna 4</h3>
+            <ul className="mt-4 space-y-4">
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base text-white hover:text-red-600 transition-all">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
