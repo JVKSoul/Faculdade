05f0b3ab42fbb24be43b2f0d83476eca0d61c240
JVKSoul <156980601+JVKSoul@users.noreply.github.com>
2024-01-25 21:30:04 -0300

    fix(Outlet):removed Footer to fix bug

--- src/pages/outlet.tsx
+++ src/pages/outlet.tsx
@@ -1,6 +1,5 @@
 import { Outlet as OutletRouterDom } from "react-router-dom";
 import { Header } from "../components/header";
-import { Footer } from "../components/footer";
 
 export default () => {
   return (
