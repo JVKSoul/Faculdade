4a16dad2429450f1cb33f2e9f01a8a6bb198659a
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-01-26 18:24:54 -0300

    feat(mock): create component productFilter and implement from page App

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Outlet from './pages/outlet';
 import Product from './pages/product';
-// import Outlet from '@components/Outlet';
-// import Home from '@pages/Home';
+import ProductFilter from './pages/productFilter';
 
 
 const App: React.FC = () => {
@@ -14,6 +13,7 @@ const App: React.FC = () => {
                     <Route path="/" element={<Outlet/>} >
                         <Route index element={<Home />} />
                         <Route path="/product/:id" element={<Product/>} />
+                        <Route path="/:filter" element={<ProductFilter/>} />
                     </Route>
                 </Routes>
         </>
---
+++ src/pages/productFilter.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IProduct } from "../../mock/interface";
+import FAKE_DATA from "../../mock/FAKE_DATA";
+import Product from "../components/product";
+
+const ProductFilter = () => {
+    const [products, setProducts] = useState<IProduct[]>();
+    const { filter } = useParams();
+
+    useEffect(() => {
+        setProducts([])
+        setProducts(FAKE_DATA.filter((product) => product.filter.includes(`${filter}`)));
+    }, [filter]);
+  return (
+    <div className="bg-zinc-900  min-h-screen flex flex-col justify-center">
+      <div className="flex flex-col justify-center items-center py-10">
+        <h2 className="self-start ml-32 text-2xl">{filter?.toUpperCase()  === "COMPUTER"? "Computador" : filter?.toUpperCase() === "PERIPHERALS" ? "Periféricos" : filter?.toUpperCase() === "HARDWARE" ? "Hardware" : "CARREGANDO..."}</h2>
+        <div className='self-start ml-32 border-t-2 border-red-600 mb-1 mt-2 w-24'></div>
+        <div className="w-4/5 mx-auto flex flex-row flex-wrap gap-5 mt-10">
+          {products &&
+            products.map((product) => {
+              return (
+                <Product key={product.id} id={product.id} product={product as unknown as IProduct} />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductFilter
